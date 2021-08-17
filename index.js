2f1b04de4bbb252a3beccc921659e870bf74889a
Altechzilla <8274669+altechzilla@users.noreply.github.com>
2021-08-17 17:13:58 +0300

    ds9

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import compose from 'compose-function';
-import { withDestackedDatabase } from '@destacked/database';
-import { withDestackedMembers } from '@destacked/members';
-import { withDestackedInterface } from '@destacked/interface';
-import { withDestackedBlocks } from '@destacked/blocks';
-import { withDestackedMarketing } from '@destacked/marketing';
-import { withDestackedAffiliates } from '@destacked/affiliates';
-import { withDestackedSupport } from '@destacked/support';
-import { withDestackedPay } from '@destacked/pay';
-import { withDestackedGamification } from '@destacked/gamification';
+// import { withDestackedDatabase } from '@destacked/database';
+// import { withDestackedMembers } from '@destacked/members';
+// import { withDestackedInterface } from '@destacked/interface';
+// import { withDestackedBlocks } from '@destacked/blocks';
+// import { withDestackedMarketing } from '@destacked/marketing';
+// import { withDestackedAffiliates } from '@destacked/affiliates';
+// import { withDestackedSupport } from '@destacked/support';
+// import { withDestackedPay } from '@destacked/pay';
+// import { withDestackedGamification } from '@destacked/gamification';
 
 import { withDestackedCore, useDestackedCore } from './provider';
 
@@ -23,14 +23,14 @@ const Destacked = ({ children }) => children;
 
 export { useDestackedCore };
 export default compose(
-    withDestackedCore,
-    withDestackedDatabase,
-    withDestackedMembers,
-    withDestackedInterface,
-    withDestackedBlocks,
-    withDestackedMarketing,
-    withDestackedAffiliates,
-    withDestackedSupport,
-    withDestackedPay,
-    withDestackedGamification
+    withDestackedCore
+    // withDestackedDatabase,
+    // withDestackedMembers,
+    // withDestackedInterface,
+    // withDestackedBlocks,
+    // withDestackedMarketing,
+    // withDestackedAffiliates,
+    // withDestackedSupport,
+    // withDestackedPay,
+    // withDestackedGamification
 )(Destacked);
